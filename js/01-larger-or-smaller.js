be346389416c895d5f401b3185d7228d8cb81138
David W <111401603+sciwalk@users.noreply.github.com>
2024-03-14 07:34:30 -0700

    Update 01-larger-or-smaller.js

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -2,4 +2,4 @@
 const integer1 = parseInt(prompt)("Enter first of 2 integers")
 
 // get second of two integers
-const integer1 = parseInt(prompt)("Enter second of 2 integers")
+const integer2 = parseInt(prompt)("Enter second of 2 integers")
