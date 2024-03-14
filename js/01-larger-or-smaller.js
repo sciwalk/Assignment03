e9f899c7ae4ef0fd029ba719a963c0dc587ee6ad
David W <111401603+sciwalk@users.noreply.github.com>
2024-03-14 07:55:07 -0700

    Update 01-larger-or-smaller.js

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,5 +1,13 @@
 //get first of two integers
-const integer1 = parseInt(prompt)("Enter first of 2 integers")
+const firstInteger = parseInt(prompt("Enter first of 2 integers"))
 
 // get second of two integers
-const integer2 = parseInt(prompt)("Enter second of 2 integers")
+const secondInteger = parseInt(prompt("Enter second of 2 integers"))
+
+if (firstInteger == secondInteger) {
+    alert("The two integers are equal")
+} else if (firstInteger > secondInteger) {
+    alert(firstInteger + " is the largest integer") 
+} else {
+    alert(secondInteger + " is the largest integer")
+}
