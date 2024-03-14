6b71f5d88179c2fec93182a4b75461068a8e28b5
David W <111401603+sciwalk@users.noreply.github.com>
2024-03-14 08:16:20 -0700

    Update 01-larger-or-smaller.js

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -4,10 +4,12 @@ const firstInteger = parseInt(prompt("Enter first of 2 integers"))
 // get second of two integers
 const secondInteger = parseInt(prompt("Enter second of 2 integers"))
 
-if (firstInteger == secondInteger) {
-    alert("The two integers are equal")
+if ((isNaN(firstInteger))||(isNaN(secondInteger))) {
+    document.write( "Two numbers were not entered")
+} else if (firstInteger == secondInteger) {
+    document.write("The two integers are equal")
 } else if (firstInteger > secondInteger) {
-    alert(firstInteger + " is the largest integer") 
+    document.write(firstInteger + " is the largest integer") 
 } else {
-    alert(secondInteger + " is the largest integer")
+    document.write(secondInteger + " is the largest integer")
 }
